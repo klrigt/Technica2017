28afa6bdc27e26ff4888523e3c23e889ed224ba7
evaevaeva <evatsai0509@gmail.com>
2017-11-05 11:44:09 -0500

    Provider Notification
    Added the pop up notification for the provider when receiver requested
    service

--- app/src/main/java/com/example/krodrigo13/technica2017/MainMenu.java
+++ app/src/main/java/com/example/krodrigo13/technica2017/MainMenu.java
@@ -1,17 +1,61 @@
 package com.example.krodrigo13.technica2017;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    private int quantity;
+    private String cellPhone;
+    private String email;
+    private double distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button SL = (Button) findViewById(R.id.button11);
+
+        SL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quantity = 1;
+                cellPhone = "123-456-7890";
+                email = "emilyle123@gmail.com";
+                distance = 0.5;
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("She needs you!");
+                builder.setMessage("Emily Le needs " + quantity + " pads!\n"
+                        + "Her contact information is " + cellPhone + " and "
+                        + email +"\nShe is " + distance + " miles away from you. Please help her!");
+
+                builder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        startActivity(new Intent(MainMenu.this, MainMenu.class));
+                    }
+                });
+
+                builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(MainMenu.this, scannerpage.class));
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 
     public void signOut(View view)
--- app/src/main/java/com/example/krodrigo13/technica2017/SignIn.java
+++ app/src/main/java/com/example/krodrigo13/technica2017/SignIn.java
@@ -11,38 +11,29 @@ import android.widget.Toast;
 
 public class SignIn extends AppCompatActivity
 {
-    EditText username = (EditText) findViewById(R.id.username);
-    EditText password = (EditText) findViewById(R.id.password);
-    ImageButton signinBut = (ImageButton) findViewById(R.id.signinbutton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        Button signinBut = (Button) findViewById(R.id.signinbutton);
+
         signinBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!(username.getText().toString().trim().equals(""))
-                        && password.getText().toString().trim().equals("")){
-                    startActivity(new Intent(SignIn.this, MainMenu.class));
-                    Toast.makeText(getApplicationContext(), "Signing in"
-                            , Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "Please enter a valid username and " +
-                            "password", Toast.LENGTH_SHORT).show();
-                }
+                startActivity(new Intent(SignIn.this, MainMenu.class));
             }
         });
     }
 
-    /*
+
     public void enterMain(View view)
     {
         Intent intent = new Intent(this, MainMenu.class);
         startActivity(intent);
     }
-    */
+
 }
